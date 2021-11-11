7c4918449bbb058355f3cc44d1e259afc75833f6
wiktorpp <grazynapp65@gmail.com>
2021-11-11 15:30:40 +0100

    commit

--- omebot.py
+++ omebot.py
@@ -9,6 +9,15 @@ def tags_str_to_dict(input):
         if tag.startswith("-"):
             output["block"].add(tag[1:])
     return output
-            
+
+def tags_dict_to_str(input):
+    output=""
+    for tag in input["I_am"]:
+        output += f">{tag} "
+    for tag in input["looking_for"]:
+        output += f"<{tag} "
+    for tag in input["block"]:
+        output += f"-{tag} "
+    return output
 
 import pdb; pdb.set_trace()
