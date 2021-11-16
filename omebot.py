4ddab69433bf9a4c2e5854eb171d7533f52b74f7
wiktorpp <grazynapp65@gmail.com>
2021-11-16 12:43:37 +0100

    wip

--- omebot.py
+++ omebot.py
@@ -1,3 +1,7 @@
+priority_weight = {"low": 1, "med":2, "high":3}
+tag_types={">": "I_am", "<": "looking_for", "-": "block"}
+class Block: pass
+
 def tags_str_to_dict(input):
     input = input.split()
     output = {"I_am": set(), "looking_for": set(), "block": set()}
@@ -27,13 +31,52 @@ def count_points(my_tags, his_tags):
     his_tags = tags_str_to_dict(his_tags)
 
     if my_tags["I_am"] & his_tags["block"]:
-        return None
+        raise Block
     if his_tags["I_am"] & my_tags["block"]:
-        return None
+        raise Block
     
     my_score = len(my_tags["looking_for"] & his_tags["I_am"])
     his_score = len(my_tags["I_am"] & his_tags["looking_for"])
 
     return (my_score, his_score)
+
+def count_points_with_priority(my_tags, his_tags):
+    my_final_result = 0
+    his_final_result = 0
+    for my_priority in ["low", "med", "high"]:
+        for his_priority in ["low", "med", "high"]:
+            my_result, his_result = count_points(my_tags[my_priority], his_tags[his_priority])
+            my_result = my_result * priority_weight[my_priority]
+            his_result = his_result * priority_weight[his_priority]
+            my_final_result += my_result
+            his_final_result += his_result
+            print(f"{my_result} {his_result}")
+    return (my_final_result, his_final_result)
+        
+empty_user={"low": "", "med": "", "high": ">f <h"}
+
+def remove_tags(tags, tags_to_remove):
+    tags_to_remove_dict = tags_str_to_dict(tags_to_remove)
+    for tag_type in ["I_am", "looking_for", "block"]:
+        tags_to_remove_dict[tag_type] = tags_dict[tag_type] - tags[tag_type]
+    user[priority] = tags_dict_to_str(tags_dict)
+
+def remove_tag_with_priority(user, tags):
+    tags = tags_str_to_dict(tags)
+    for priority in ["low", "med", "high"]:
+        user[priority] = remove_tags(user[priority], tags)
+    return user
+
+def add_tag_with_priority(user, tags, priority):
+    user = remove_tag(user, tags)
+    tags = tags_str_to_dict(tags)
+    print(tags)
+    print(user[priority])
+    user_dict = tags_str_to_dict(user[priority])
+    print(user_dict)
+    for tag_type in ["I_am", "looking_for", "block"]:
+        user_dict[tag_type] = set.union(user_dict[tag_type], tags[tag_type])
+    user[priority] = tags_dict_to_str(user[priority])
+    return user
 
 import pdb; pdb.set_trace()
